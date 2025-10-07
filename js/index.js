c387954f9da79ef7121cf003168f09609505af83
tony <antoniussophy@gmail.com>
2025-10-07 21:59:23 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// window.alert('tonyy')
+// document.getElementById('demo').innerHTML="hello word"
+// console.log('hcdbc')
+
+// var test = ""
+// for(var i=0 ; i<100 ; i++){
+//     test+="<h2>Hello</h2>";
+// }
+
+// document.getElementById("demo").innerHTML= test;
+
+function getsum(){
+    var num1= 15;
+    var num2= 15;
+    console.log(num1+num2);
+}
+function getavg(num1,num2){
+    var avg = num1 + num2
+    console.log(avg/2);
+}
+
+getsum()
+getavg(15,10)
